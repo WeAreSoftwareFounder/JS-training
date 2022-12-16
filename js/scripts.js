61b7f38aa54688b64cb4ac543d9300ca1ac5e055
Kirk Wilkinson <35461641+WeAreSoftwareFounder@users.noreply.github.com>
2022-12-16 13:02:51 +0000

    Simplified loop code

--- js/scripts.js
+++ js/scripts.js
@@ -1,66 +1,27 @@
 let  pokemonList = [
 
- {
-  name: "Bulbasor",
-  height: 0.7,
-  type: ["grass"]
-},
+  {name: "Bulbasor", height: 0.7, types: "grass" },
   
-  {
-  name: "Charizard",
-  height: 1.7,
-  type: ["fire", "flying"]
-},
+  {name: "Charizard", height: 1.7, types: "fire" },
 
-  {
-  name: "Squirtle",
-  height: 1,
-  type: ["water"]
-},
+  {name: "Squirtle", height: 1, types: "water" }
+
 ];
 
-function Bulbasor(pokemon){
+for(let i= 0; i <= pokemonList.length; i++){
 
-  return pokemon.name = "Bulbasor" || "Charzard"|| "Squirtal";
-  return pokemon.type = 'grass' ||  "fire" || "water";
-  return pokemon.height =  "0.7" || "1.7" || "0.5";
+  document.writeln(pokemonList[i].name);
+  document.writeln(pokemonList[i].height);
+  document.writeln(pokemonList[i].types);
 
-}
-function Charizard(pokemon){
+  if(pokemonList[i].height >= 1.1){
 
-  return pokemon.name = "Squirtal";
-  return pokemon.type = "water";
-  return pokemon.height = "0.5";
+    document.writeln("Wow charzard is big");
 
-}
-function Squirtal(pokemon){
+  }else{
 
-  return pokemon.name =  "Charzard";
-  return pokemon.type = "fire" ;
-  return pokemon.height = "1.7";
+    document.writeln("god these are tiny");
 
-}
-function findHeight(pokemon){
+  }
 
-  return pokemon.height = "1.0";
-
-}
-for(let i = 0; i <= pokemonList.length; i++)[
-
-  document.writeln(pokemonList.find(Bulbasor).name),
-  document.writeln(pokemonList.find(Bulbasor).height),
-  document.writeln(pokemonList.find(Bulbasor).type),
-  document.writeln(pokemonList.find(Squirtal).name),
-  document.writeln(pokemonList.find(Bulbasor).height),
-  document.writeln(pokemonList.find(Bulbasor).type),
-  document.writeln(pokemonList.find(Charizard).name),
-  document.writeln(pokemonList.find(Charizard).height),
-  document.writeln(pokemonList.find(Charizard).type),
-  
-
-]
-if (pokemonList.find(findHeight) .height >= 1.0 && pokemonList.find(Charizard) .name){
-   document.writeln("so big");
-} else {
-    document.writeln("so small")
 }
