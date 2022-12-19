3b0d73f9a957b7a288a02b812db10bc2b2f3fc82
Kirk Wilkinson <35461641+WeAreSoftwareFounder@users.noreply.github.com>
2022-12-19 14:30:11 +0000

    Created forEach Loop
    embedded array within  an IIFE and created call-back functions to  allow access to the array.
    
    created and utilized a forEach loop to run through the call back function accessing the array

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,49 @@
-let  pokemonList = [
+// Creates pokemoneRepositry IFFE
+let  pokemonRepository= (function(){
 
-  {name: "Bulbasor", height: 0.7, types: "grass" },
+  let pokemonList =  [
+
+    {name: "Bulbasor", height: 0.7, types: "grass" },
   
-  {name: "Charizard", height: 1.7, types: "fire" },
+    {name: "Charizard", height: 1.7, types: "fire" },
 
-  {name: "Squirtle", height: 1, types: "water" }
+    {name: "Squirtle", height: 1, types: "water" },
 
-];
+]
 
-for(let i= 0; i <= pokemonList.length; i++){
+// add pokemon to pokemoneRepositroy IIFE
+return {
 
-  document.writeln(pokemonList[i].name);
-  document.writeln(pokemonList[i].height);
-  document.writeln(pokemonList[i].types);
+  add: function(pokemone){
 
-  if(pokemonList[i].height >= 1.1){
+    pokemonList.push(pokemone);
 
-    document.writeln("Wow charzard is big");
+  },
 
-  }else{
+  //return all values of pokemonRepositry IIFE
+  getAll: function(){
 
-    document.writeln("god these are tiny");
+    return pokemonList;
+
+  },
+
+}
+
+return {
+
+  add : add,
+  getAll: getAll,
+
+};
+
+})();
+  // Searches array for objects and prints to documment
+  function Search(){
+
+    pokemonRepository.add({name: 'Pikachu', height: 0.3, types: "Electric" });
+    pokemonRepository.getAll().forEach(Element => document.writeln('<p>' + Element.name + ' ', Element.height + ' ', Element.types + ' ', '</p>'))
 
   }
 
-}
+// print object array to document loop
+pokemonRepository.getAll().forEach(Element => document.writeln(Search()));
