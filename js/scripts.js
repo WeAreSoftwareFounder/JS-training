cd17e692071f94c13cb5379131cf06c832cccc90
Kirk Wilkinson <35461641+WeAreSoftwareFounder@users.noreply.github.com>
2023-01-07 14:17:49 +0000

    Created fetch function for pokemonlist

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,6 @@
 let  pokemoneRepositroy =  (function(){
 
-  let pokemonList = [
-
-    {name: "Bulbasor", height: 0.7, types: "grass" },
-  
-    {name: "Charizard", height: 1.7, types: "fire" },
-
-    {name: "Squirtle", height: 1, types: "water" },
-
-  ]
+  let pokemonList = []
   //retuns pokelist array
   function getAll(){
 
@@ -28,6 +20,7 @@ let  pokemoneRepositroy =  (function(){
     let listPokemon = document.createElement('li');
     let button = document.createElement('button');
     button.classList.add("button-class");
+    button.innerText = pokemon.name;
     listPokemon.appendChild(button);
     pokemonList.appendChild(listPokemon);
 
@@ -39,10 +32,34 @@ let  pokemoneRepositroy =  (function(){
     })
 
   };
+
+  function loadList(){
+
+    return fetch('https://pokeapi.co/api/v2/pokemon/').then(function(response){
+      return response.json();
+    }).then(function(json){
+      json.results.forEach(function(item){
+        let pokemon = {
+          name: item.name,
+          showDetailsuRL: item.url,
+          height: item.height,
+          imgUrl: item.imgUrl
+        }
+        add(pokemon);
+      })
+    }).catch(function(e){
+      console.log(e);
+    })
+  }
+  
   //prints pokemonlist array to console when called
   function showDetails(pokemon){
 
-    console.log(pokemon.name);
+    loadList(pokemon).then(function(){
+
+      console.log(pokemon);
+
+    })
 
   };
   
@@ -51,16 +68,22 @@ let  pokemoneRepositroy =  (function(){
     getAll:  getAll,
     add: add,
     addlistitem: addlistitem,
-    showDetails: showDetails
+    showDetails: showDetails,
+    loadList: loadList
 
   };
 
 })();
 
-pokemoneRepositroy.getAll().forEach(function(pokemon){
+pokemoneRepositroy.loadList().then(function(){
 
-  pokemoneRepositroy.addlistitem(pokemon);
+  pokemoneRepositroy.getAll().forEach(function(pokemon){
 
+    pokemoneRepositroy.addlistitem(pokemon);
   
+    
+  
+  })
+
 
 })
