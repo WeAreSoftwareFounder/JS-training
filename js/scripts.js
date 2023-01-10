343667f8be884235b07626346a26ec15e367e882
Kirk Wilkinson <35461641+WeAreSoftwareFounder@users.noreply.github.com>
2023-01-10 16:08:28 +0000

    amended script errors

--- js/scripts.js
+++ js/scripts.js
@@ -1,89 +1,111 @@
 let  pokemoneRepositroy =  (function(){
 
-  let pokemonList = []
-  //retuns pokelist array
-  function getAll(){
+let pokemonList = []
+//retuns pokelist array
+function getAll(){
 
-    return pokemonList;
+  return pokemonList;
 
-  }
-  //pushed new pokemon to array
-  function add(pokemon){
+}
+//pushed new pokemon to array
+function add(pokemon){
 
-    pokemonList.push(pokemon);
+  pokemonList.push(pokemon);
 
-  }
-  // created and implemnts buttons for onlclick function to run showDetails function
-  function addlistitem(pokemon){
 
-    let pokemonList = document.querySelector('.pokemon-list');
-    let listPokemon = document.createElement('li');
-    let button = document.createElement('button');
-    button.classList.add("button-class");
-    button.innerText = pokemon.name;
-    listPokemon.appendChild(button);
-    pokemonList.appendChild(listPokemon);
+}
+// created and implemnts buttons for onlclick function to run showDetails function
+function addlistitem(pokemon){
 
-    button.addEventListener('click', function display(){
+  let pokemonList = document.querySelector('.pokemon-list');
+  let listPokemon = document.createElement('li');
+  let button = document.createElement('button');
+  button.classList.add("button-class");
+  button.innerText = pokemon.name;
+  listPokemon.appendChild(button);
+  pokemonList.appendChild(listPokemon);
 
-      pokemoneRepositroy.showDetails(pokemon);
+  button.addEventListener('click', function display(){
+
+    pokemoneRepositroy.showDetails(pokemon);
+  
+
+  })
+
+};
+
+function loadList(){
+
+  return fetch('https://pokeapi.co/api/v2/pokemon/').then(function(response){
+    return response.json();
+  }).then(function(json){
+    json.results.forEach(function(item){
     
+      let pokemon = {
+        name: item.name,
+        showDetailsUrl: item.url,
+      }
 
+      add(pokemon);
     })
+  }).catch(function(e){
+    console.log(e);
+  })
+}
 
-  };
+function loadDetails(pokemon){
 
-  function loadList(){
+  let url = pokemon.showDetailsUrl;
+  return fetch(url).then(function(response){
 
-    return fetch('https://pokeapi.co/api/v2/pokemon/').then(function(response){
-      return response.json();
-    }).then(function(json){
-      json.results.forEach(function(item){
-        let pokemon = {
-          name: item.name,
-          showDetailsuRL: item.url,
-          height: item.height,
-          imgUrl: item.imgUrl
-        }
-        add(pokemon);
-      })
-    }).catch(function(e){
-      console.log(e);
-    })
-  }
-  
-  //prints pokemonlist array to console when called
-  function showDetails(pokemon){
+    return response.json();
 
-    loadList(pokemon).then(function(){
+  }).then(function(details){
 
-      console.log(pokemon);
+    pokemon.imageUrl = details.sprites.front_default;
+    pokemon.height = details.height;
+    pokemon.types = details.types;
 
-    })
+  }).catch(function(e){
 
-  };
-  
-  return {
+    console.log(e);
 
-    getAll:  getAll,
-    add: add,
-    addlistitem: addlistitem,
-    showDetails: showDetails,
-    loadList: loadList
+  })
 
-  };
+}
+
+//prints pokemonlist array to console when called
+function showDetails(pokemon){
+
+  loadDetails(pokemon).then(function(){
+
+    console.log(pokemon)
+
+  })
+
+}
+
+return {
+
+  getAll:  getAll,
+  add: add,
+  addlistitem: addlistitem,
+  showDetails: showDetails,
+  loadList: loadList,
+  loadDetails: loadDetails,
+  showDetails: showDetails
+
+};
 
 })();
 
 pokemoneRepositroy.loadList().then(function(){
 
-  pokemoneRepositroy.getAll().forEach(function(pokemon){
+pokemoneRepositroy.getAll().forEach(function(pokemon){
 
-    pokemoneRepositroy.addlistitem(pokemon);
-  
-    
-  
-  })
+  pokemoneRepositroy.addlistitem(pokemon);
+
+})
 
 
 })
