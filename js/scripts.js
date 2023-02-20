3e919c10b7eac531fa5eb491fec861d2035ef358
WeAreSoftwareFounder <35461641+WeAreSoftwareFounder@users.noreply.github.com>
2023-02-20 19:30:10 +0000

    Modified code for implementation of bootstrap framwork

--- js/scripts.js
+++ js/scripts.js
@@ -17,21 +17,23 @@ function add(pokemon){
 // created and implemnts buttons for onlclick function to run showDetails function
 function addListItem(pokemon){
 
-  let pokemonList = document.querySelector('.pokemon-list');
+  let pokemonList = document.querySelector('.list-group');
   let listPokemon = document.createElement('li');
+  listPokemon.classList.add('list-group-item')
   let button = document.createElement('button');
-  button.classList.add("button-class");
+  button.classList.add("btn", "btn-info");
   button.innerText = pokemon.name;
   listPokemon.appendChild(button);
   pokemonList.appendChild(listPokemon);
 
-  button.addEventListener('click', function display(){
+  button.addEventListener('click', function () {
 
-  showModal('Name: ' + pokemon.name, 'Height: ' + pokemon.height, 'Image: ' + pokemon.imageUrl);
-  showDetails(pokemon);
-  console.log(pokemon);
+    document.querySelector('.modal-title').innerText = pokemon.name;
+    document.querySelector('.modal-body').innerText = pokemon.height;
+    document.getElementsByClassName('img').src = pokemon.imageUrl;
+    
 
-  });
+  })
 
 };
 
@@ -71,60 +73,6 @@ function showDetails(pokemon) {
   loadDetails(pokemon);
 }
 
-//prints pokemonlist array to console when called
-
-function showModal(title, text, img) {
-  let modalContainer = document.querySelector('#modal-container');
-
-  // Clear all existing modal content
-  modalContainer.innerHTML = '';
-
-  let modal = document.createElement('div');
-  modal.classList.add('modal');
-
-  // Add the new modal content
-  let closeButtonElement = document.createElement('button');
-  closeButtonElement.classList.add('modal-close');
-  closeButtonElement.innerText = 'Close';
-  closeButtonElement.addEventListener('click', hideModal);
-
-  let titleElement = document.createElement('h1');
-  titleElement.innerText = title;
-
-  let contentElement = document.createElement('h2');
-  contentElement.innerText = text;
-
-  let imageElement = document.createElement('img');
-  imageElement.innerHTML = img;
-
-  modal.appendChild(closeButtonElement);
-  modal.appendChild(titleElement);
-  modal.appendChild(contentElement);
-  modal.appendChild(imageElement);
-  modalContainer.appendChild(modal);
-
-
-
-  modalContainer.classList.add('is-visible');
-}
-
-
-function hideModal() {
-  let modalContainer = document.querySelector('#modal-container');
-  modalContainer.classList.remove('is-visible');
-
-  let closeButtonElement = document.createElement('button');
-  closeButtonElement.classList.add('modal-close');
-  closeButtonElement.innerText = 'Close';
-  closeButtonElement.addEventListener('click', hideModal);
-}
-window.addEventListener('keydown', (e) => {
-  let modalContainer = document.querySelector('#modal-container');
-  if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
-    hideModal();  
-  }
-});
-
 
 return {
 
@@ -134,8 +82,6 @@ return {
   addListItem: addListItem,
   loadList: loadList,
   loadDetails: loadDetails,
-  showModal: showModal,
-  hideModal: hideModal
 
 };
 
@@ -147,6 +93,7 @@ pokemoneRepositroy.getAll().forEach(function(pokemon){
 
   pokemoneRepositroy.addListItem(pokemon);
   pokemoneRepositroy.showDetails(pokemon);
+  
   
 })
 
